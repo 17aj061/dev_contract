42d51c58fc6ae0b6722be794b8f492cca884eef8
17aj061 <shuuuu@outlook.jp>
2020-11-12 16:29:37 +0900

    success Auth!

--- confilm.js
+++ confilm.js
@@ -3,7 +3,7 @@ const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
 
 const fs = require('fs');
-const { keccak256 } = require('web3-utils');
+const {soliditySha3} = require('web3-utils');
 const jsonObject1 = JSON.parse(fs.readFileSync('./build/contracts/SimpleStorage.json','utf8'));
 const jsonObject2 = JSON.parse(fs.readFileSync('./build/contracts/Authentication.json','utf8'));
 
@@ -19,7 +19,7 @@ const contract2 = new web3.eth.Contract(contract_abi2, contract_address2);
 web3.eth.defaultAccount='0x4a393CD5175000947Fe11226A1333d63837cC863';
 
 async function add_person(size){
-    const hex;
+    let hex = '';
     for(let i = 0; i < size; i++){
         let random = Math.round(Math.random() * 4);
         switch(random){
@@ -32,13 +32,21 @@ async function add_person(size){
             case 3:
                 hex = await contract.methods.set("bob",32).send({from: web3.eth.defaultAccount,gas:3000000}).then();
         }
+        await send_tx_hash(hex.transactionHash.substr(2));
     }
-    await send_tx_hash(hex.transactionHash.substr(2));
     //get_person_all();
 }
 
 async function send_tx_hash(hex){
     const hash = await contract2.methods.set_tx(hex,web3.eth.defaultAccount).send({from: web3.eth.defaultAccount,gas:3000000}).then();
+    fs.appendFileSync('./privatekey', hex + '\n', 'utf-8');
+}
+
+async function auth(address){
+    let contents = fs.readFileSync('./privatekey', 'utf-8').split('\n');
+    let hash = soliditySha3(contents[contents.length - 2], contents[contents.length - 3], contents[contents.length - 4], contents[contents.length - 5], contents[contents.length - 6]);
+    const isSuccess = await contract2.methods.auth(hash, address).call();
+    console.log(isSuccess);
 }
 
 async function get_person(name){
@@ -74,6 +82,7 @@ async function get_person_all(){
 
 //fucntion save_Tx()
 
-//add_person(2);
+//add_person(6);
+auth(web3.eth.defaultAccount);
 //get_person('alice');
-get_person_all();
+//get_person_all();
--- file_test.js
+++ file_test.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
+/*
 const hash = 'b1c83adf223b8abbeca2506efc5a875cd0d0810b2aa9fc1df8823360a1b537df' + '\n';
 fs.appendFileSync('./privatekey', hash, 'utf-8');
+*/
 
-let contents = fs.readFileSync('./privatekey', 'utf-8');
-let lines = contents.split('\n')
-console.log(lines.length-1);
-for(let i = 1;i <= 10;i++){
-    console.log(lines[lines.length - i]);
+let contents = fs.readFileSync('./privatekey', 'utf-8').split('\n');
+for(let i = 1;i <= 5;i++){
+    console.log(contents[contents.length - i - 1]);
 }
