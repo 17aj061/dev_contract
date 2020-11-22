71a3d83de478ec588d478332cd676f59f82a7c68
17aj061 <shuuuu@outlook.jp>
2020-11-22 18:00:57 +0900

    Auto ssh login to VM

---
+++ login2vm.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start ssh -i ./raft.pem azureuser@104.215.20.53
+start ssh -i ./raft.pem azureuser@104.215.20.248
+start ssh -i ./raft.pem azureuser@104.215.20.56
+start ssh -i ./raft.pem azureuser@104.215.20.64
+start ssh -i ./raft.pem azureuser@104.215.20.130
+start ssh -i ./raft.pem azureuser@104.215.21.27
+start ssh -i ./raft.pem azureuser@104.215.9.146
+start ssh -i ./raft.pem azureuser@104.215.20.99
+start ssh -i ./raft.pem azureuser@104.215.21.5
+start ssh -i ./raft.pem azureuser@104.215.21.33
